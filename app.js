998003c578de35360f16b03a5e10e6fcda90a58e
Artem Deikun <artem.deikun@zorallabs.com>
2016-12-17 00:11:07 +0200

    #3 child element

--- app.js
+++ app.js
@@ -1,12 +1,16 @@
 angular.module('myApp', ['pascalprecht.translate', 'ui.bootstrap'])
     .config(function ($translateProvider) {
         $translateProvider.translations('en', {
-            WELCOME: 'Hey. Welcome to the app!',
-            SUBSCRIPTION: 'Today, our product have already signed {{count}} people'
+            INFO: {
+                WELCOME: 'Hey. Welcome to the app!',
+                SUBSCRIPTION: 'Today, our product have already signed {{count}} people'
+            }
         })
             .translations('de', {
-                WELCOME: 'Hallo. Willkommen in der App',
-                SUBSCRIPTION: 'Heute haben unser Produkt bereits unterzeichnet {{count}} Personen'
+                INFO: {
+                    WELCOME: 'Hallo. Willkommen in der App',
+                    SUBSCRIPTION: 'Heute haben unser Produkt bereits unterzeichnet {{count}} Personen'
+                }
             });
         $translateProvider.preferredLanguage('en');
     });
